c09ea63cb01487d00c1d70d22961b396113bcccb
Belyaev, Evgeniy <evgeniy.belyaev@arcadia.spb.ru>
2020-10-31 19:40:15 +0300

    logout

--- src/components/facebookLogin.tsx
+++ src/components/facebookLogin.tsx
@@ -10,11 +10,10 @@ export const FacebookLogin = ({ onLoggedIn }: Props) => {
             if (response.status === "connected") {
                 onLoggedIn(response.status)
             }
-        })    
+        })
     })
 
     return (<>
-        <div id="fb-root"></div>
-        <div className="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>
+        <div className="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="true" data-use-continue-as="true" data-width=""></div>
     </>)
 }
